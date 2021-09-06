f9d91dc0ecb7c8869416a1dac3317bfcd5058808
ichiru matsuda <ichiru1222@gmail.com>
2021-09-07 00:03:37 +0900

    修正

--- get_follower.py
+++ get_follower.py
@@ -14,22 +14,30 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth,wait_on_rate_limit = True)
 
 user = input("「@ユーザー名」を入力してください：")
-followerIDs = api.followers_ids(user)
 
+
+followers_ids = tweepy.Cursor(api.followers_ids, id=user, cursor=-1).items()
 followerDatas = []
-for followerID in followerIDs:
-    followerData = {}
-    data = api.get_user(followerID)
-    followerData["Name"] = data.name
-    followerData["Follow"] = data.friends_count
-    followerData["Follower"] = data.followers_count
-    followerData["Description"] = data.description
-    followerData["TweetCount"] = data.statuses_count
-    followerDatas.append(followerData)
+for i, follower_id in enumerate(followers_ids):
+    try:
+        followerData = {}
+        data = api.get_user(follower_id)
+        followerData["Name"] = data.name
+        followerData["User_id"] = data.screen_name
+        followerData["Location"] = data.location
+        followerData["URL"] = data.url
+        followerData["Follow"] = data.friends_count
+        followerData["Follower"] = data.followers_count
+        followerData["Description"] = data.description
+        followerData["TweetCount"] = data.statuses_count
+        followerDatas.append(followerData)
+        print(followerData)
+        print("count:{}".format(i))
+    except tweepy.error.TweepError as e:
+        print(e.reason)
 
 pd.set_option("display.max_rows", 1000)
-df = pd.DataFrame(followerDatas).loc[:,["Name","Follow","Follower","TweetCount","Description"]]
-
+df = pd.DataFrame(followerDatas).loc[:,["Name", "User_id", "URL", "Location","Follow","Follower","TweetCount","Description"]]
 #ファイル出力
 fileName = input("ファイル名を入力してください：")
 df.to_csv(fileName + ".csv")
